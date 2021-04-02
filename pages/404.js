6de97b1a7a06838a0e0993a1b6152889eccf0c91
YouDontKnowThis <fox.physics@outlook.fr>
2021-04-02 14:15:48 +0200

    first commit

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PageNotFound() {
+  return (
+    <div>
+      <h1>This is the 404 error page</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
---
+++ pages/blog/[blogId].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BlogPost() {
+  return (
+    <div>
+      <h1>This is the blog post</h1>
+    </div>
+  );
+}
+
+export default BlogPost;
